0763a74bc70da332c23cf750e141f8f153537cd9
Chris Talkington <chris@talkingtontech.com>
2015-11-19 16:50:53 -0600

    config(usebanner): remove extraneous spacing.

--- config/grunt/usebanner.js
+++ config/grunt/usebanner.js
@@ -4,18 +4,18 @@ module.exports = {
     options: {
       position: 'top',
       banner: '/*!\n' +
-        'Theme Name:  <%= pkg.theme.name %>\n' +
-        'Version:     <%= pkg.version %>\n' +
-        'Author:      <%= pkg.theme.author %>\n' +
-        'Author URI:  <%= pkg.theme.authoruri %>\n' +
-        'Theme URI:   <%= pkg.theme.uri %>\n' +
+        'Theme Name: <%= pkg.theme.name %>\n' +
+        'Version: <%= pkg.version %>\n' +
+        'Author: <%= pkg.theme.author %>\n' +
+        'Author URI: <%= pkg.theme.authoruri %>\n' +
+        'Theme URI: <%= pkg.theme.uri %>\n' +
         'Description: <%= pkg.theme.description %>\n<% if (pkg.theme.template.length > 0) { %>' +
         'Template: <%= pkg.theme.template %>\n<% } %><% if (pkg.theme.templateversion.length > 0) { %>' +
         'Template Version: <%= pkg.theme.templateversion %>\n<% } %>' +
-        'Tags:        <%= pkg.theme.tags %>\n' +
+        'Tags: <%= pkg.theme.tags %>\n' +
         'Text Domain: <%= pkg.theme.textdomain %>\n' +
         'Domain Path: <%= pkg.theme.domainpath %>\n' +
-        'License:     <%= pkg.theme.license %>\n' +
+        'License: <%= pkg.theme.license %>\n' +
         'License URI: <%= pkg.theme.licenseuri %>\n' +
         '*/\n',
       linebreak: true
